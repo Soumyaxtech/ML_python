5d80a2fda85b9d29b600008dca73eb052b8f64c4
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-08-21 23:59:37 +0530

    basic

--- basics/datatype.py
+++ basics/datatype.py
@@ -1,8 +1,42 @@
+#   DATATYPES AND VARIABLE IN PYTHON............
+
 a = 56      # int 
-print(a)
+e = 34
+e = a
+a = 90
+print(a,e)
 b="soumyajit"       # string
 print(b)
 c= 5.9      # float
 print(c)
 d=True
 print(d)
+
+#   PRINTING IN PYTHON ............
+
+print("koley",45,"end",end='\n')
+print("name")
+
+
+# USER INPUT IN PYTHON...............
+
+#name = input("name : ")
+#age = input("age : ")
+
+#print (name ,age)
+
+# ARITHMETIC OPERATOR................
+
+x = 4
+y = 3.3 # arithmetic operation can be performed bw int float
+
+print(x+y)
+print(x-y)
+print(int (x*y))    # int (operation) only gives int part
+print(x/y)
+print(x**y)     # x to the power y
+print(x//y)     # gives only int result
+
+
+num = input("number : ")
+print()
