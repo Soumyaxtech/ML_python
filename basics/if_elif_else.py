8b94f068f6ae08d4a26aa6c74460f32cd2939ed2
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-08-28 12:00:53 +0530

    basic if else statement

---
+++ basics/if_elif_else.py
@@ -0,0 +1,12 @@
+x = input("enter name : ")
+if(x=="soumya"):{
+    print("you are good ")
+}
+elif(x=="babai"):{
+    print("write original name ")
+}
+else:{
+    print ("who are you?")
+}
+
+# can use mul elif statement 
