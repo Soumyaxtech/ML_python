fcd9e6b7a62302a5fd64bc6b71854ef373c55040
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-08-28 11:06:43 +0530

    string and its operations(basic)

--- basics/string.py
+++ basics/string.py
@@ -1,2 +1,27 @@
 str = "soumya"
-print(type(str))
+print(type(str))    # type of variable
+
+#   STRING METHODS .....................
+
+greet = "heLLo World"
+
+print (greet.upper())   # all capital
+print (greet.lower())   # all small
+print (greet.capitalize())  # 1st cap then all small
+
+print(greet.count("ll"))    # o/p ---> 0    count num of alphabet
+print(greet.count("o"))     # o/p ---> 2
+
+# MUL OF STRING WITH INT .................
+
+x="hello "
+y=3
+print(x*y)  # o/p ---> hello hello hello
+
+# ADDITION ....................................
+
+g = "hey "
+k = "babai"
+print(g+k)  # string concatination
+
+
